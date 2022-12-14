964453c351aeed91a7176f181dcb1aca4374061f
tamulavage <tamulavage@gmail.com>
2022-12-13 19:54:22 -0500

    clean up code

--- UDPClientSend.ino
+++ UDPClientSend.ino
@@ -1,37 +1,34 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <WiFiUdp.h>
-#include <Arduino_LSM6DS3.h>
+#include <Arduino_LSM6DSOX.h>
 #include "secrets.h"
 
-#define A_BUTTON 4
-#define B_BUTTON 3
+#define A_BUTTON_PIN 4
+#define B_BUTTON_PIN 3
 #define POT_PIN A1
-#define heart_beat_pin   LED_BUILTIN
+#define HEARTBEAT_PIN   LED_BUILTIN
 
-#define pot_enabled true
-#define button_enabled true
-#define wifi_enabled true
+#define POT_ENABLED true
+#define BUTTONS_ENABLED true
+#define WIFI_ENABLED true
+#define IMU_ENABLED true
 
-const long unsigned heart_beat_freq = 1000; // time(milliseconds) of heart beat frequency
-long countOfRunTime = 0;
-long unsigned heart_beat_on_off_time; // the time the LED is on and off - 1/2 frequency
-long unsigned last_heart_beat_time;   // time in milliseconds of last heart beat status change
-bool heart_beat_status = HIGH;        // current status of heart beat, start high
-
+const long unsigned heartBeatOnTime  = 500 ;// time(milliseconds) of heart beat time
+long unsigned lastHeartBeatTime;   // time in milliseconds of last heart beat status change
+bool heartBeatStatus = HIGH;        // current status of heart beat, start high
 
 int status = WL_IDLE_STATUS;
 const char ssid[] = SECRET_SSID;
 const char pass[] = SECRET_PASS;
 const IPAddress serverAddress(192, 168, 1, 200);
 
-unsigned int localPort = 2390;
-//char packetBuffer[256];
+const unsigned int localPort = 2390;
 char  clientMsg[25] = "";
 WiFiUDP Udp;
 
-int A_BUTTONstate = 0;
-int B_BUTTONstate = 0;
+int buttonAState = 0;
+int buttonBState = 0;
 int potValue = 0;
 int lastPotValue = 0;
 const int sensitivityPOT = 35;
@@ -39,10 +36,10 @@ const int sensitivityPOT = 35;
 int convertRoll, convertPitch;
 float lastAy = 0;
 float lastAz = 0;
-const float sensitivityAcc = .05;
+const float sensitivityAcc = .15;
 
 float Ax, Ay, Az;
-boolean angleChanged = false;
+bool angleChanged = false;
 
 void setup() {
 
@@ -51,75 +48,45 @@ void setup() {
   GPIOSetUp();
   printRed();
 
-  if (!IMU.begin()) {
-    Serial.println("\IMU Chip not initializing...");
-    while (1);
+  if (IMU_ENABLED) {
+    IMUSetup();
   }
-  Serial.println("\IMU Chip setup completed...");
 
-  if(wifi_enabled) {
+  if (WIFI_ENABLED) {
     wifiSetUp();
     printBlue();
     wifiConnect();
   }
 
-
   Udp.begin( localPort);
-  Serial.println("\nConnection completed...");
-
-  pinMode(heart_beat_pin, OUTPUT);
-  heart_beat_on_off_time = heart_beat_freq / 2; // LED is on and off at 1/2 frequency time
-
 
   printGreen();
   Serial.println("\Set up completed...");
 }
 
 void loop () {
-  
+
   heart_beat();
-  int lastA_ButtonState = A_BUTTONstate;
-  int lastB_ButtonState = B_BUTTONstate;
 
-  A_BUTTONstate = digitalRead(A_BUTTON);
-  B_BUTTONstate = digitalRead(B_BUTTON);
-  potValue = analogRead(POT_PIN);
-
-  if (pot_enabled && (potValue > (lastPotValue + sensitivityPOT) || potValue <  (lastPotValue - sensitivityPOT))) {
-    sprintf(clientMsg, "--POT%d", potValue);
-    sendMsg(clientMsg) ;
-    lastPotValue = potValue;
+  if (POT_ENABLED) {
+    processPOT();
   }
 
-  if (button_enabled) {
-    if (A_BUTTONstate == HIGH && A_BUTTONstate != lastA_ButtonState)
-    {
-      strcpy(clientMsg, "--BUTTONA_ON");
-      sendMsg(clientMsg) ;
-    }
-
-    if (A_BUTTONstate == LOW && A_BUTTONstate != lastA_ButtonState)
-    {
-      strcpy(clientMsg, "--BUTTONA_OFF");
-      sendMsg(clientMsg) ;
-    }
-
-    if (B_BUTTONstate == HIGH && B_BUTTONstate != lastB_ButtonState)
-    {
-      strcpy(clientMsg, "--BUTTONB_ON");
-      sendMsg(clientMsg) ;
-    }
-
-    if (B_BUTTONstate == LOW && B_BUTTONstate != lastB_ButtonState)
-    {
-      strcpy(clientMsg, "--BUTTONB_OFF");
-      sendMsg(clientMsg) ;
-    }
+  if (BUTTONS_ENABLED) {
+    processButtons();
   }
 
+  if (IMU_ENABLED) {
+    processIMU();
+  }
+}
+
+void processIMU() {
   if (IMU.accelerationAvailable()) {
+    delay(30); // addd or overloads chip which would freeze aplication
     IMU.readAcceleration(Ax, Ay, Az);
     angleChanged = false;
+
     if (Ay > (lastAy + sensitivityAcc) || Ay < (lastAy - sensitivityAcc)) {
       angleChanged = true;
     }
@@ -142,21 +109,60 @@ void loop () {
   }
 }
 
+void processPOT() {
+  potValue = analogRead(POT_PIN);
+
+  if (potValue > (lastPotValue + sensitivityPOT) || potValue <  (lastPotValue - sensitivityPOT)) {
+    sprintf(clientMsg, "--POT%d", potValue);
+    sendMsg(clientMsg) ;
+    lastPotValue = potValue;
+  }
+}
+
+void processButtons() {
+
+  int lastbuttonAState = buttonAState;
+  int lastbuttonBState = buttonBState;
+
+  buttonAState = digitalRead(A_BUTTON_PIN);
+  buttonBState = digitalRead(B_BUTTON_PIN);
+
+  if (buttonAState != lastbuttonAState)
+  {
+    if (buttonAState == HIGH) {
+      strcpy(clientMsg, "--BUTTONA_ON");
+    }
+    else {
+      strcpy(clientMsg, "--BUTTONA_OFF");
+    }
+    sendMsg(clientMsg) ;
+  }
+
+  if (buttonBState != lastbuttonBState)
+  {
+    if (buttonBState == HIGH) {
+      strcpy(clientMsg, "--BUTTONB_ON");
+    }
+    else {
+      strcpy(clientMsg, "--BUTTONB_OFF");
+    }
+    sendMsg(clientMsg) ;
+  }
+}
+
 int calculateRoll(float y, float z) {
-//  roll = atan2(y, z) * 180 / PI;
   return (atan2(y, z) * 180 / PI) * 10;
 }
 
 int calculatePitch(float x, float y, float z) {
-//  pitch = atan2(-x, sqrt(y * y + z * z)) * 180 / PI;
   return (atan2(-x, sqrt(y * y + z * z)) * 180 / PI) * 10;
 }
+
 
 void printWifiStatus() {
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-//  IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println( WiFi.localIP());
 
@@ -181,12 +187,23 @@ void wifiConnect() {
   printWifiStatus();
 }
 
+void IMUSetup() {
+  if (!IMU.begin()) {
+    Serial.println("\IMU Chip not initializing...");
+    while (1);
+  }
+  Serial.println("\IMU Chip setup completed...");
+}
+
 void GPIOSetUp() {
   pinMode(LEDR, OUTPUT);
   pinMode(LEDG, OUTPUT);
   pinMode(LEDB, OUTPUT);
-  pinMode(A_BUTTON, INPUT);
-  pinMode(B_BUTTON, INPUT);
+  pinMode(HEARTBEAT_PIN, OUTPUT);
+  
+  pinMode(A_BUTTON_PIN, INPUT);
+  pinMode(B_BUTTON_PIN, INPUT);
+  pinMode(POT_PIN, INPUT);
 }
 
 void wifiSetUp() {
@@ -209,8 +226,7 @@ void sendMsg(char clientMsg[]) {
   Udp.write(clientMsg);
   Udp.endPacket();
   Udp.flush();
-  //delay(100);
-  countOfRunTime=0;
+  delay(20); // minor delaty to not overload chip/udp/receiver 
 }
 
 void printGreen() {
@@ -232,14 +248,10 @@ void printRed() {
 }
 
 void heart_beat() {
-  if (millis() - last_heart_beat_time >= heart_beat_on_off_time) {
+  if (millis() - lastHeartBeatTime >= heartBeatOnTime) {
     // time to swap status of the heart beat LED and update it
-    last_heart_beat_time = millis();
-    heart_beat_status = !heart_beat_status;           // invert current heart beat status value
-    digitalWrite(heart_beat_pin, heart_beat_status);  // update LED with new status
-    if(heart_beat_status){
-      countOfRunTime++;
-      Serial.println(countOfRunTime);
-    }
+    lastHeartBeatTime = millis();
+    heartBeatStatus = !heartBeatStatus;           // invert current heart beat status value
+    digitalWrite(HEARTBEAT_PIN, heartBeatStatus);  // update LED with new status
   }
 }
